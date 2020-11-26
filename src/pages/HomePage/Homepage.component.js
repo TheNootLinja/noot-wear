5384fcc369b21fb43a6e35296ecfb8707197ec0e
TheNootLinja <thenootlinja@gmail.com>
2020-11-26 17:03:30 -0500

    Add routing to our menu-item components for navigating using withRouter HOC

--- src/pages/HomePage/Homepage.component.js
+++ src/pages/HomePage/Homepage.component.js
@@ -2,7 +2,7 @@ import './Homepage.styles.scss'
 
 import Directory from '../../components/Directory/Directory.component';
 
-const HomePage = () => (
+const HomePage = ({ history }) => (
     <div className='homepage'>
         <Directory />
     </div>
