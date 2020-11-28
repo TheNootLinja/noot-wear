dd2fdfdbff5eea26bec66a16c65692e4c9b7ff0d
TheNootLinja <thenootlinja@gmail.com>
2020-11-27 20:47:29 -0500

    Add start of SignInUpPage and the components and styles that go with it

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage/Homepage.component';
-import ShopPage from './pages/ShopPage/ShopPage.component.js';
+import ShopPage from './pages/ShopPage/ShopPage.component';
 import Header from './components/Header/Header.component';
+import SignInUpPage from './pages/SignInUpPage/SignInUpPage.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/SignInUpPage/SignInUpPage.component.js
@@ -0,0 +1,9 @@
+import './SignInUpPage.styles.scss';
+
+import SignIn from '../../components/SignIn/SignIn.component';
+
+const SignInUpPage = () => (
+    <div className='sign-in-and-sign-up'><SignIn/></div>
+);
+
+export default SignInUpPage;
