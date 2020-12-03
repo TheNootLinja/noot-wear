e89ab4847cf273b05725aa923aa35528efc38b52
TheNootLinja <thenootlinja@gmail.com>
2020-12-02 22:29:56 -0500

    Add signup component for creating a new account, along with the code for sending the new user account to the firestore

--- src/pages/SignInUpPage/SignInUpPage.component.js
+++ src/pages/SignInUpPage/SignInUpPage.component.js
@@ -1,9 +1,13 @@
 import './SignInUpPage.styles.scss';
 
 import SignIn from '../../components/SignIn/SignIn.component';
+import SignUp from '../../components/SignUp/SignUp.component';
 
 const SignInUpPage = () => (
-    <div className='sign-in-and-sign-up'><SignIn/></div>
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+        <SignUp/>
+    </div>
 );
 
 export default SignInUpPage;
