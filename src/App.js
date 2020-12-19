b5f4dc064e7bc9b41a9199553640893c225ce9ea
TheNootLinja <thenootlinja@gmail.com>
2020-12-19 17:26:08 -0500

    Implement userReducer and userActions using redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
 
 import HomePage from './pages/HomePage/Homepage.component';
@@ -7,37 +9,29 @@ import ShopPage from './pages/ShopPage/ShopPage.component';
 import Header from './components/Header/Header.component';
 import SignInUpPage from './pages/SignInUpPage/SignInUpPage.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
+
+        // Code for setting currentUser state
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-      // Need to rememebr that in an instance where we want
-      // console.log the state right after using setState, we need to
-      // use a callback function as the second argument to the setState
-      // function to get back accurate data.
           });
         });
       }
       else {
-      this.setState({currentUser: userAuth})
+      setCurrentUser(userAuth);
       }
     });
   }
@@ -60,4 +54,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
