4b700d230afb29227620dca5c3e24135c8e74430
Nicholas Peters <thenootlinja@gmail.com>
2021-01-20 20:51:53 -0500

    Add code that closes the cart dropdown component when the user navigates to the checkout page

--- src/components/CartDropdown/CartDropdown.component.js
+++ src/components/CartDropdown/CartDropdown.component.js
@@ -5,10 +5,11 @@ import { withRouter } from 'react-router-dom';
 import CustomButton from '../CustomButton/CustomButton.component';
 import CartItem from '../CartItem/CartItem.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './CartDropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
    <div className='cart-dropdown'>
        <div className='cart-items'>
            {
@@ -20,7 +21,12 @@ const CartDropdown = ({ cartItems, history }) => (
                <span className='empty-message'>Your cart is empty</span>
                )}
        </div>
-       <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+       <CustomButton onClick={() => {
+           history.push('/checkout');
+           dispatch(toggleCartHidden());
+        }}>
+            GO TO CHECKOUT
+        </CustomButton>
    </div> 
 )
 
