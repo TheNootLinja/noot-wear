088d345d5c7d59be9e9a49d23c27ce52b9ec229c
Nicholas Peters <thenootlinja@gmail.com>
2021-01-21 21:10:25 -0500

    Add ability to increase or decrease the quantity of an item in the checkout

--- src/components/CheckoutItem/CheckoutItem.component.js
+++ src/components/CheckoutItem/CheckoutItem.component.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
 
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
 import './CheckoutItem.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity} = cartItem;
     return(
     <div className='checkout-item'>
@@ -12,14 +12,20 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
             <img src={imageUrl} alt='item' />
         </div>
         <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
+        <span className='quantity'>
+            <div onClick={() => removeItem(cartItem)} className='arrow'>&#10094;</div>
+            <span className='value'>{quantity}</span>
+            <div onClick={() => addItem(cartItem)} className='arrow'>&#10095;</div>
+        </span>
         <span className='price'>{price}</span>
         <div onClick={() => clearItem(cartItem)} className='remove-button'>&#10008;</div>
     </div>
 )}
 
 const mapDispatchToProps = dispatch => ({
-    clearItem: item => dispatch(clearItemFromCart(item))
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
 })
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
